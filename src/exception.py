edc638665b7f4c96032f1ca7411ac32f4dbc320d
Vaibhav Gupta <vg498660@gmail.com>
2024-10-27 19:41:13 +0530

    Minor change

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys ## Tells us about the problems encountered by the python runtime environment 
 
+from src.logger import logging
+
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
